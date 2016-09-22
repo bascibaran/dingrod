2145bbd85fd39a7fbee8a65ce8b599ae25c93cfc
bascibaran <baran.balkan@gmail.com>
2016-09-22 09:58:53 -0700

    added caching requests, to limit irods pokes

--- server.py
+++ server.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python
+import datetime   as dt
 import dingpoker  as dp
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
 PORT_NUMBER = 20500
 
+
 class pingHandler(BaseHTTPRequestHandler):
+  lastping  = None
+  ding      = None
 
   def do_GET(self):
-    ding = dp.poke()
-    if ding is not None:
+    if self.lastping is None:
+      self.lastping = dt.datetime.now()
+      self.ding = dp.poke()
+    else:
+      tnow = dt.datetime.now()
+      tdelt = tnow - lastping
+      if tdelt.total_seconds > 1:
+        self.lastping = tnow
+        self.ding = dp.poke()
+    if self.ding is not None:
     #return 200 and json object stored as string in ding
       self.send_response(200)
       self.send_header('Content-Type','application/json')
       self.end_headers()
-      self.wfile.write(ding)
+      self.wfile.write(self.ding)
     else:
     #return 503
       self.send_response(503)
